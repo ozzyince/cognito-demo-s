706a5f592ca540063b0167b46c693ea5c3a03735
Özgür İnce <ozgur@ince.ist>
2020-01-30 00:28:17 +0300

    modify cors config

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,15 @@ var router = express.Router()
 var cors = require('cors')
 
 /* GET users listing. */
-router.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-router.all('*', cors());
-router.options('*', cors())
+// router.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Credentials', true)
+//   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+//   res.header('Access-Control-Allow-Headers', 'authorization, Origin, X-Requested-With, Content-Type, Accept')
+//   next()
+// })
+// router.all('*', cors());
+// router.options('*', cors())
 router.get('/', function (req, res, next) {
   // Here we can check the req.user.scope array contains the scope relevant for the REST API operation being invoked
   res.send('Successfully verified JWT token. Extracted information: ' + JSON.stringify(req.user))
