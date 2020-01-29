537977a1f60f9858fee87ede2d20f1dfce8af9d1
Özgür İnce <ozgur@ince.ist>
2020-01-29 11:33:26 +0300

    modify

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-var port = process.env.PORT || 3000;
-var express = require('express');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var cors = require('cors')
 
-var app = express();
+var usersRouter = require('./routes/users')
+var cognitoAuth = require('./lib/cognitoAuth')
+const appConfig = require('./config/app-config.json')
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
-});
+var app = express()
 
-app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
-});
+// Configure CORS for this service so our UI can make calls to us.
+var corsOptions = {
+  origin: [appConfig.signoutUri]
+}
+app.use(cors(corsOptions))
 
-app.listen(port);
-module.exports = app;
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+const cognitoAuthMiddleware = cognitoAuth.getVerifyMiddleware()
+app.use('/users', cognitoAuthMiddleware, usersRouter)
+
+module.exports = app
