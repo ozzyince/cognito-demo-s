60a4681139a55b3beac63028f741422343186670
Özgür İnce <ozgur@ince.ist>
2020-01-29 23:49:08 +0300

    modify cors config

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'authorization, Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 app.use(logger('dev'))
